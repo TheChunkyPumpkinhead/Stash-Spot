0d9272e307ddff15a21378f5bd59cf96addcd46e
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-08-21 22:52:29 -0700

    fixed product.controller

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -9,4 +9,8 @@ import {
 
 const router = express.Router();
 router.get("/", getProducts);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
 export default router;
